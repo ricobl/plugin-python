a84ad308018d83e7428139e7c65ac737ac3c03bf
Enrico <rico.bl@gmail.com>
2015-10-07 13:52:42 -0300

    Disabled file completion on `pycd` and `pyversion`

--- completions/python.fish
+++ completions/python.fish
@@ -5,5 +5,5 @@ function __fish_python_available_modules
   python -c "import pkgutil; print '\n'.join([m[1] for m in pkgutil.iter_modules() if m[1].startswith('$q')])"
 end
 
-complete --no-files -c pycd -a "(__fish_python_available_modules)"
-complete --no-files -c pyversion -a "(__fish_python_available_modules)"
+complete --no-files -A -c pycd -a "(__fish_python_available_modules)"
+complete --no-files -A -c pyversion -a "(__fish_python_available_modules)"
